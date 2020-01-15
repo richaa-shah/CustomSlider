97524ddc489c62ff50405d9d9cb7a065e9df0080
richaa-shah <16167921+richaa-shah@users.noreply.github.com>
2020-01-15 14:14:55 +0000

    Create slider functionality

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  var slider = document.getElementById("myRange");
+  var output = document.getElementById("demo");
+  output.innerHTML = slider.value; // Display the default slider value
+
+  // Update the current slider value (each time you drag the slider handle)
+  slider.oninput = function() {
+    output.innerHTML = this.value;
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>Custom Range Slider</h1>
+      <div className="slidecontainer">
+        <input
+          type="range"
+          min="1"
+          max="100"
+          defaultValue="50"
+          className="slider"
+          id="myRange"
+        />
+        <p>
+          Value: <span id="demo" />
+        </p>
+      </div>
     </div>
   );
 }
